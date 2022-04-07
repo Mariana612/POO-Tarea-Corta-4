2d66491d061a99f076cb1f28ffb54409e430ecf3
Mariana12 <mariananavarro3022@gmail.com>
2022-04-06 19:30:40 -0600

    Finished

--- src/Frame.java
+++ src/Frame.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class Frame extends JFrame implements ActionListener {
 
@@ -12,14 +13,19 @@ public class Frame extends JFrame implements ActionListener {
 
     Frame(){
 
+        JLabel tittle = new JLabel("HOLA PROFE");
+        tittle.setBounds(160,50,100,50);
+
         //Botones
-        button = new JButton();
+        button = new JButton("1");
         button.setBounds(150,125,100,50);
         button.addActionListener(this);
-        button2 = new JButton();
+
+        button2 = new JButton("2");
         button2.setBounds(150,225,100,50);
         button2.addActionListener(this);
-        button3 = new JButton();
+
+        button3 = new JButton("3");
         button3.setBounds(150,325,100,50);
         button3.addActionListener(this);
 
@@ -35,27 +41,39 @@ public class Frame extends JFrame implements ActionListener {
         this.add(button);
         this.add(button2);
         this.add(button3);
+        this.add(tittle);
     }
-    private void lookForException(int num){
-        if (this.num != num){
-            System.out.println("error");
+    private void lookForException(int num) {
+        try{
+            if (this.num == num){
+            }
+            else{throw new IOException();}
+        }
+        catch (Exception e) {
+            JLabel aText = new JLabel("<html> Botones presionados en desorden. <br/> Presiono "  + num + " en vez de " + this.num + ".",JLabel.CENTER);
+            JDialog error = new JDialog();
+            error.setTitle("Botones presionados en desorden.");
+            error.add(aText, BorderLayout.CENTER);
+            error.setVisible(true);
+            error.setSize(260,100);
+            error.setLocation(200,200);
+            error.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+
             this.num = 0;
         }
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         this.num += 1;
         if(e.getSource()==button){
-            System.out.println(1);
             lookForException(1);
         }
         if(e.getSource()==button2){
-            System.out.println(2);
             lookForException(2);
         }
         if(e.getSource()==button3) {
-            System.out.println(3);
             lookForException(3);
             this.num = 0;
         }
